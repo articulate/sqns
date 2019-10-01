b5c50e564dcd90836a230acf7bcf7155cce85196
Patrick Klingemann <patrick.klingemann@gmail.com>
2019-10-01 17:15:52 -0400

    make tests pass

--- index.js
+++ index.js
@@ -1,73 +1,79 @@
 const AWS = require('aws-sdk')
-AWS.config.update({ region: process.env.AWS_REGION })
-const sqs = new AWS.SQS()
-const sns = new AWS.SNS({ apiVersion: '2010-03-31' })
-const { promisify } = require('@articulate/funky')
+const { promisify } = require('util')
 const { path, prop } = require('ramda')
 
-const createQueue = promisify(sqs.createQueue, sqs)
-const getQueueAttributes = promisify(sqs.getQueueAttributes, sqs)
-const setQueueAttributes = promisify(sqs.setQueueAttributes, sqs)
-const subscribe = promisify(sns.subscribe, sns)
-const setSubscriptionAttributes = promisify(sns.setSubscriptionAttributes, sns)
-
-const createDeadletterQueue = (user, queueName) =>
-  createQueue({ QueueName: `${queueName}-${user}-DLQ` })
-    .then(prop('QueueUrl'))
-
-const getQueueArn = QueueUrl =>
-  getQueueAttributes({
-    QueueUrl,
-    AttributeNames: ['QueueArn'],
-  }).then(path(['Attributes', 'QueueArn']))
-
-const createEventsQueue = ({ deadletterQueueArn, user, queueName }) =>
-  createQueue({
-    Attributes: {
-      RedrivePolicy: JSON.stringify({
-        deadLetterTargetArn: deadletterQueueArn,
-        maxReceiveCount: 3,
-      }),
-    },
-    QueueName: `${queueName}-${user}`,
-  }).then(prop('QueueUrl'))
-
-const setEventsQueueAttributes = (queueUrl, queueArn, snsTopic) =>
-  setQueueAttributes({
-    Attributes: {
-      Policy: JSON.stringify({
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Effect: 'Allow',
-            Principal: '*',
-            Action: 'SQS:SendMessage',
-            Resource: queueArn,
-            Condition: {
-              ArnEquals: {
-                'aws:SourceArn': snsTopic,
-              },
-            },
-          },
-        ],
-      }),
-    },
-    QueueUrl: queueUrl,
-  })
-
-const createTopicSubscription = (queueArn, topicArn) =>
-  subscribe({
-    Endpoint: queueArn,
-    Protocol: 'sqs',
-    TopicArn: topicArn,
-  }).then(prop('SubscriptionArn'))
-
-const sqns = async (options) => {
+const sqns = async (options = {}) => {
   const {
+    region,
     user,
     queueName,
     topic = {},
   } = options
+
+  AWS.config.update({ region })
+
+  if (!region) throw Error('Missing region')
+  if (!user) throw Error('Missing user')
+  if (!queueName) throw Error('Missing queueName')
+
+  const sqs = new AWS.SQS()
+  const sns = new AWS.SNS({ apiVersion: '2010-03-31' })
+  const createQueue = promisify(sqs.createQueue.bind(sqs))
+  const getQueueAttributes = promisify(sqs.getQueueAttributes.bind(sqs))
+  const setQueueAttributes = promisify(sqs.setQueueAttributes.bind(sqs))
+  const subscribe = promisify(sns.subscribe.bind(sns))
+  const setSubscriptionAttributes = promisify(sns.setSubscriptionAttributes.bind(sns))
+
+  const createDeadletterQueue = (user, queueName) =>
+    createQueue({ QueueName: `${queueName}-${user}-DLQ` })
+      .then(prop('QueueUrl'))
+
+  const getQueueArn = QueueUrl =>
+    getQueueAttributes({
+      QueueUrl,
+      AttributeNames: ['QueueArn'],
+    }).then(path(['Attributes', 'QueueArn']))
+
+  const createEventsQueue = ({ deadletterQueueArn, user, queueName }) =>
+    createQueue({
+      Attributes: {
+        RedrivePolicy: JSON.stringify({
+          deadLetterTargetArn: deadletterQueueArn,
+          maxReceiveCount: 3,
+        }),
+      },
+      QueueName: `${queueName}-${user}`,
+    }).then(prop('QueueUrl'))
+
+  const setEventsQueueAttributes = (queueUrl, queueArn, snsTopic) =>
+    setQueueAttributes({
+      Attributes: {
+        Policy: JSON.stringify({
+          Version: '2012-10-17',
+          Statement: [
+            {
+              Effect: 'Allow',
+              Principal: '*',
+              Action: 'SQS:SendMessage',
+              Resource: queueArn,
+              Condition: {
+                ArnEquals: {
+                  'aws:SourceArn': snsTopic,
+                },
+              },
+            },
+          ],
+        }),
+      },
+      QueueUrl: queueUrl,
+    })
+
+  const createTopicSubscription = (queueArn, topicArn) =>
+    subscribe({
+      Endpoint: queueArn,
+      Protocol: 'sqs',
+      TopicArn: topicArn,
+    }).then(prop('SubscriptionArn'))
 
   const deadletterQueueUrl = await createDeadletterQueue(user, queueName)
   const deadletterQueueArn = await getQueueArn(deadletterQueueUrl)
