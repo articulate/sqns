9fdb104bd4d4a106ff7efed7085c121297fb6d2a
Patrick Klingemann <patrick.klingemann@gmail.com>
2019-11-06 16:00:49 -0500

    Code (#1)
    * initial commit
    
    * tests
    
    * make tests pass
    
    * setSDKInstance
    
    * remove unnecessary expectation
    
    * add README
    
    * remove references to user
    
    * more accurate usage docs
    
    * casing
    
    * more casing
    
    * @articulate/sqns
    
    * make RawMessageDelivery default to true
    
    * review feedback
    
    * Events is too specific to our use case
    
    * make maxReceiveCount configurable
    
    * add maxReceiveCount option and example usage to README
    
    * add travis
    
    * travis try harder
    
    * 6?! nah
    
    * -:ci
    
    * const

---
+++ README.md
@@ -0,0 +1,45 @@
+# SQNS
+
+An SQS queue creator and SNS subscriber
+
+## Usage
+
+```haskell
+sqns :: { k: v } -> Promise String
+```
+
+Call `sqns` to create an SQS queue (and deadletter queue). You will be returned the queue URL of the newly created SQS queue. Optionally subscribe the queue to an SNS topic via the options below.
+
+## Options
+
+`sqns` accepts a JSON object with the following options:
+
+| Option | Type | Default | Description |
+| ------ |----- |-------- |------------ |
+| `region` | `String` | | AWS region |
+| `queueName` | `String` | | an identifier for the queue |
+| `maxReceiveCount` | `Number` | 3 | `maxReceiveCount` for the queue redrive policy |
+| `topic` | `object` | `{ rawMessageDelivery: true }` | SNS Topic Subscription options (see below) |
+
+### SNS Topic Subscription Options
+
+| Option | Type | Default | Description |
+| ------ | ---- | ------- | ----------- |
+| `arn` | `string` | | SNS Topic ARN |
+| `filterPolicy` | `Object` | | [SNS Topic Subscription Filter Policy](https://docs.aws.amazon.com/en_pv/sns/latest/dg/sns-subscription-filter-policies.html) |
+| `rawMessageDelivery` | `Boolean` | `true` | [SNS Topic Subscription Raw Message Delivery](https://docs.aws.amazon.com/sns/latest/dg/sns-large-payload-raw-message-delivery.html) |
+
+## Example Usage
+
+```javascript
+const sqns = require('sqns')
+
+const queueUrl = sqns({
+  region: 'us-east-1',
+  queueName: `{username}-events`,
+  maxReceiveCount: 1,
+  topic: {
+    arn: topicArn,
+  }
+})
+```
---
+++ test/index.js
@@ -0,0 +1,284 @@
+const AWS = require('aws-sdk-mock')
+const sinon = require('sinon')
+const path = require('path')
+AWS.setSDK(path.resolve('./node_modules/aws-sdk-mock'))
+AWS.setSDKInstance(require('aws-sdk'))
+
+const sqns = require('../')
+
+describe('sqns', () => {
+  context('when region option is not provided', () => {
+    it('rejects with an error', () =>
+      expect(sqns()).to.be.rejectedWith(Error, 'Missing region')
+    )
+  })
+
+  context('when queueName option is not provided', () => {
+    it('rejects with an error', () =>
+      expect(sqns({ region: 'us-east-1' })).to.be.rejectedWith(Error, 'Missing queueName')
+    )
+  })
+
+  context('when region, queueName options are provided', () => {
+    let createQueueStub
+    let getQueueAttributesStub
+
+    beforeEach(() => {
+      createQueueStub = sinon.stub()
+      createQueueStub
+        .onCall(0)
+        .callsArgWith(1, null, { QueueUrl: 'mock-deadletter-queue-url' })
+        .onCall(1)
+        .callsArgWith(1, null, { QueueUrl: 'mock-queue-url' })
+      getQueueAttributesStub = sinon.stub()
+      getQueueAttributesStub
+        .onCall(0)
+        .callsArgWith(1, null, { Attributes: { QueueArn: 'mock-deadletter-queue-arn' } })
+        .onCall(1)
+        .callsArgWith(1, null, { Attributes: { QueueArn: 'mock-queue-arn' } })
+      setQueueAttributesStub = sinon.stub()
+      setQueueAttributesStub
+        .callsArgWith(1, null, {  })
+      AWS.mock('SQS', 'createQueue', createQueueStub)
+      AWS.mock('SQS', 'getQueueAttributes', getQueueAttributesStub)
+    })
+
+    afterEach(() => {
+      AWS.restore('SQS', 'createQueue')
+      AWS.restore('SQS', 'getQueueAttributes')
+    })
+
+    it('creates a deadletter queue', async () => {
+      const queueUrl = await sqns({
+        region: 'us-east-1',
+        queueName: 'queue',
+      })
+      expect(createQueueStub).to.have.been.calledWith({ QueueName: 'queue-DLQ' })
+      expect(createQueueStub).to.have.been.calledWith({
+        QueueName: 'queue',
+        Attributes: {
+          RedrivePolicy: JSON.stringify({
+            deadLetterTargetArn: 'mock-deadletter-queue-arn',
+            maxReceiveCount: 3,
+          })
+        }
+      })
+      expect(queueUrl).to.equal('mock-queue-url')
+    })
+
+    it('creates a deadletter queue with customMaxReceiveCount', async () => {
+      const queueUrl = await sqns({
+        region: 'us-east-1',
+        queueName: 'queue',
+        maxReceiveCount: 1,
+      })
+      expect(createQueueStub).to.have.been.calledWith({ QueueName: 'queue-DLQ' })
+      expect(createQueueStub).to.have.been.calledWith({
+        QueueName: 'queue',
+        Attributes: {
+          RedrivePolicy: JSON.stringify({
+            deadLetterTargetArn: 'mock-deadletter-queue-arn',
+            maxReceiveCount: 1,
+          })
+        }
+      })
+      expect(queueUrl).to.equal('mock-queue-url')
+    })
+
+    context('when topic arn is provided', () => {
+      let setQueueAttributes
+      let subscribeStub
+      let setSubscriptionAttributesStub
+
+      beforeEach(() => {
+        setQueueAttributesStub = sinon.stub()
+        setQueueAttributesStub
+          .callsArgWith(1, null, { QueueUrl: 'mock-queue-url' })
+        subscribeStub = sinon.stub()
+        subscribeStub
+          .callsArgWith(1, null, { SubscriptionArn: 'mock-subscription-arn' })
+        setSubscriptionAttributesStub = sinon.stub()
+        setSubscriptionAttributesStub
+          .onCall(0)
+          .callsArgWith(1, null, { })
+        AWS.mock('SQS', 'setQueueAttributes', setQueueAttributesStub)
+        AWS.mock('SNS', 'subscribe', subscribeStub)
+        AWS.mock('SNS', 'setSubscriptionAttributes', setSubscriptionAttributesStub)
+      })
+
+      afterEach(() => {
+        AWS.restore('SQS', 'setQueueAttributes')
+        AWS.restore('SNS', 'subscribe')
+        AWS.restore('SNS', 'setSubscriptionAttributes')
+      })
+
+      it('creates a topic subscription', async () => {
+        const queueUrl = await sqns({
+          region: 'us-east-1',
+          queueName: 'queue',
+          topic: {
+            arn: 'mock-sns-topic-arn',
+          }
+        })
+        expect(createQueueStub).to.have.been.calledWith({ QueueName: 'queue-DLQ' })
+        expect(createQueueStub).to.have.been.calledWith({
+          QueueName: 'queue',
+          Attributes: {
+            RedrivePolicy: JSON.stringify({
+              deadLetterTargetArn: 'mock-deadletter-queue-arn',
+              maxReceiveCount: 3,
+            })
+          }
+        })
+        expect(subscribeStub).to.have.been.calledWith({
+          Endpoint: 'mock-queue-arn',
+          Protocol: 'sqs',
+          TopicArn: 'mock-sns-topic-arn',
+        })
+        expect(setQueueAttributesStub).to.have.been.calledWith({
+          Attributes: {
+            Policy: JSON.stringify({
+              Version: '2012-10-17',
+              Statement: [
+                {
+                  Effect: 'Allow',
+                  Principal: '*',
+                  Action: 'SQS:SendMessage',
+                  Resource: 'mock-queue-arn',
+                  Condition: {
+                    ArnEquals: {
+                      'aws:SourceArn': 'mock-sns-topic-arn',
+                    },
+                  },
+                },
+              ],
+            }),
+          },
+          QueueUrl: 'mock-queue-url',
+        })
+        expect(setSubscriptionAttributesStub).to.have.been.calledWith({
+          SubscriptionArn: 'mock-subscription-arn',
+          AttributeName: 'RawMessageDelivery',
+          AttributeValue: 'true',
+        })
+        expect(queueUrl).to.equal('mock-queue-url')
+      })
+
+      context('when topic filterPolicy is provided', () => {
+        beforeEach(() => {
+          setSubscriptionAttributesStub
+            .onCall(1)
+            .callsArgWith(1, null, { })
+        })
+
+        it('creates a topic subscription', async () => {
+          const queueUrl = await sqns({
+            region: 'us-east-1',
+            queueName: 'queue',
+            topic: {
+              arn: 'mock-sns-topic-arn',
+              filterPolicy: { mock: 'filter-policy' },
+            }
+          })
+          expect(createQueueStub).to.have.been.calledWith({ QueueName: 'queue-DLQ' })
+          expect(createQueueStub).to.have.been.calledWith({
+            QueueName: 'queue',
+            Attributes: {
+              RedrivePolicy: JSON.stringify({
+                deadLetterTargetArn: 'mock-deadletter-queue-arn',
+                maxReceiveCount: 3,
+              })
+            }
+          })
+          expect(subscribeStub).to.have.been.calledWith({
+            Endpoint: 'mock-queue-arn',
+            Protocol: 'sqs',
+            TopicArn: 'mock-sns-topic-arn',
+          })
+          expect(setQueueAttributesStub).to.have.been.calledWith({
+            Attributes: {
+              Policy: JSON.stringify({
+                Version: '2012-10-17',
+                Statement: [
+                  {
+                    Effect: 'Allow',
+                    Principal: '*',
+                    Action: 'SQS:SendMessage',
+                    Resource: 'mock-queue-arn',
+                    Condition: {
+                      ArnEquals: {
+                        'aws:SourceArn': 'mock-sns-topic-arn',
+                      },
+                    },
+                  },
+                ],
+              }),
+            },
+            QueueUrl: 'mock-queue-url',
+          })
+          expect(setSubscriptionAttributesStub).to.have.been.calledWith({
+            SubscriptionArn: 'mock-subscription-arn',
+            AttributeName: 'FilterPolicy',
+            AttributeValue: JSON.stringify({ mock: 'filter-policy' })
+          })
+          expect(setSubscriptionAttributesStub).to.have.been.calledWith({
+            SubscriptionArn: 'mock-subscription-arn',
+            AttributeName: 'RawMessageDelivery',
+            AttributeValue: 'true',
+          })
+          expect(queueUrl).to.equal('mock-queue-url')
+        })
+      })
+
+      context('when topic rawMessageDelivery is false', () => {
+        it('creates a topic subscription', async () => {
+          const queueUrl = await sqns({
+            region: 'us-east-1',
+            queueName: 'queue',
+            topic: {
+              arn: 'mock-sns-topic-arn',
+              rawMessageDelivery: false,
+            }
+          })
+          expect(createQueueStub).to.have.been.calledWith({ QueueName: 'queue-DLQ' })
+          expect(createQueueStub).to.have.been.calledWith({
+            QueueName: 'queue',
+            Attributes: {
+              RedrivePolicy: JSON.stringify({
+                deadLetterTargetArn: 'mock-deadletter-queue-arn',
+                maxReceiveCount: 3,
+              })
+            }
+          })
+          expect(subscribeStub).to.have.been.calledWith({
+            Endpoint: 'mock-queue-arn',
+            Protocol: 'sqs',
+            TopicArn: 'mock-sns-topic-arn',
+          })
+          expect(setQueueAttributesStub).to.have.been.calledWith({
+            Attributes: {
+              Policy: JSON.stringify({
+                Version: '2012-10-17',
+                Statement: [
+                  {
+                    Effect: 'Allow',
+                    Principal: '*',
+                    Action: 'SQS:SendMessage',
+                    Resource: 'mock-queue-arn',
+                    Condition: {
+                      ArnEquals: {
+                        'aws:SourceArn': 'mock-sns-topic-arn',
+                      },
+                    },
+                  },
+                ],
+              }),
+            },
+            QueueUrl: 'mock-queue-url',
+          })
+          expect(queueUrl).to.equal('mock-queue-url')
+        })
+      })
+    })
+  })
+})
